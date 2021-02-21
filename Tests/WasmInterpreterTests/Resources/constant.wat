8341014c179e98b7668f770e877ab97856bc0494
Anthony Drendel <me@anthonydrendel.com>
2021-02-21 15:14:38 +0100

    Update to newest 0.4.8

--- Tests/WasmInterpreterTests/Resources/constant.wat
+++ Tests/WasmInterpreterTests/Resources/constant.wat
@@ -3,4 +3,6 @@
   (func (;0;) (type 0) (result i32)
     i32.const 65536)
   (export "constant_1" (func 0))
-  (export "constant_2" (func 0)))
+  (export "constant_2" (func 0))
+  (export "constant_3" (func 0))
+  (export "constant_4" (func 0)))
--- Tests/WasmInterpreterTests/Wasm Modules/ConstantModule.swift
+++ Tests/WasmInterpreterTests/Wasm Modules/ConstantModule.swift
@@ -16,9 +16,17 @@ public struct ConstantModule {
         return Int(try _vm.call("constant_2") as Int32)
     }
 
+    func constant3() throws -> Int {
+        return Int(try _vm.call("constant_3") as Int32)
+    }
+
+    func constant4() throws -> Int {
+        return Int(try _vm.call("constant_4") as Int32)
+    }
+
     // `wat2wasm -o >(base64) Tests/WasmInterpreterTests/Resources/constant.wat | pbcopy`
     private static var wasm: [UInt8] {
-        let base64 = "AGFzbQEAAAABBQFgAAF/AwIBAAcbAgpjb25zdGFudF8xAAAKY29uc3RhbnRfMgAACggBBgBBgIAECw=="
+        let base64 = "AGFzbQEAAAABBQFgAAF/AwIBAAc1BApjb25zdGFudF8xAAAKY29uc3RhbnRfMgAACmNvbnN0YW50XzMAAApjb25zdGFudF80AAAKCAEGAEGAgAQL"
         return Array<UInt8>(Data(base64Encoded: base64)!)
     }
 }
